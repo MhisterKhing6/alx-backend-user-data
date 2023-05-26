d86d437323ae1fdf6bf8a0df41982fd522090ada
Kingsley Botchway <mhisterkhing@localhost.localdomain>
2023-05-26 03:00:41 +0000

    add

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -2,10 +2,14 @@
 """ bycrypting a password """
 import bcrypt
 
-def hash_password(password : str) -> bytes:
+
+def hash_password(password: str) -> bytes:
     """encrypt password using bcrypt """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
-def is_valid(hashed_password: bytes, password : str) -> bool:
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
     """Check for validity of a password"""
-    return True if bcrypt.checkpw(password.encode(), hashed_password) else False
+    return True if bcrypt.checkpw(
+        password.encode(),
+        hashed_password) else False

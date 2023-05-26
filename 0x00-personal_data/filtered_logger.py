667e968469139b90cc89faea29a265c60196dbca
Kingsley Botchway <mhisterkhing@localhost.localdomain>
2023-05-26 03:20:19 +0000

    h333

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -34,29 +34,50 @@ class RedactingFormatter(logging.Formatter):
 
 
     def format(self, record: logging.LogRecord) -> str:
+        """Return a formatted string """
         return filter_datum(
             self.fields, self.REDACTION, record.getMessage(), 
             self.SEPARATOR)
         
-
 def get_logger() -> logging.Logger:
-    """Get a logger """
-    user_data = logging.getLogger("user_data")
-    StreamHandler = logging.StreamHandler()
-    user_data.setLevel(logging.INFO)
-    StreamHandler.setFormatter(RedactingFormatter.FORMAT)
-    user_data.propagate = False
-    user_data.addHandler(StreamHandler)
-    return user_data
+    """
+    Returns a Logger object for handling Personal Data
+
+    Returns:
+        A Logger object with INFO log level and RedactingFormatter
+        formatter for filtering PII fields
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(stream_handler)
+
+    return logger
+
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    """ Connect to a secure database """
-    db_name = os.environ.get('PERSONAL_DATA_DB_NAME')
-    user = os.environ.get('PERSONAL_DATA_DB_USERNAME')
-    password = os.environ.get('PERSONAL_DATA_DB_PASSWORD')
-    host = os.environ.get('PERSONAL_DATA_DB_HOST')
-    return connector.connect(database=db_name, user=user, 
-                            password=password, host=host)
+    """
+    Returns a MySQLConnection object for accessing Personal Data database
+
+    Returns:
+        A MySQLConnection object using connection details from
+        environment variables
+    """
+    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = environ.get("PERSONAL_DATA_DB_NAME")
+
+    cnx = mysql.connector.connection.MySQLConnection(user=username,
+                                                     password=password,
+                                                     host=host,
+                                                     database=db_name)
+    return cnx
+
+
 
 def main() -> None:
     """ main code that obsecure certain fields"""

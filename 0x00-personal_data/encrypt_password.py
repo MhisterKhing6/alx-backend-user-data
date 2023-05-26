274fa86dcb62281316050896cbf5ceae41956416
Kingsley Botchway <mhisterkhing@localhost.localdomain>
2023-05-26 02:54:11 +0000

    how

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -4,8 +4,8 @@ import bcrypt
 
 def hash_password(password : str) -> bytes:
     """encrypt password using bcrypt """
-    return bcrypt.hashpw(password, bcrypt.gensalt())
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
 def is_valid(hashed_password: bytes, password : str) -> bool:
     """Check for validity of a password"""
-    return True if bcrypt.checkpw(password, hashed_password) else False
+    return True if bcrypt.checkpw(password.encode(), hashed_password) else False

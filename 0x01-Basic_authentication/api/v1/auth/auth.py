d4527953398686e535d67858c04cd395ec181729
Kingsley Botchway <mhisterkhing@localhost.localdomain>
2023-05-31 04:46:00 +0000

    how

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,23 +1,32 @@
 #!/usr/bin/env python3
 from flask import request
+from typing import List, TypeVar
 """Creates the auth class """
 
 
 class Auth:
     """
     Hadles authentication
+    return headers
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         Check the require paths
+        return headers
         """
         return False
 
     def authorization_header(self, request=None) -> str:
-        """ Check if the header is authorized"""
+        """ 
+        Check if the header is authorized
+        and returns headers
+        """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ current user """
+        """ 
+        current user 
+        the user
+        """
         return None

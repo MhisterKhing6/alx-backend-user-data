ec7335528868a737b3263388072dc53781d765e4
Kingsley Botchway <mhisterkhing@localhost.localdomain>
2023-06-02 09:01:05 +0000

    how

---
+++ 0x02-Session_authentication/api/v1/app.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+from os import getenv
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+if os.environ.get("AUTH_TYPE"):
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+
+
+def beforet():
+    """
+    Handles operations before request
+    """
+    request.current_user = auth.current_user(request)
+    if auth:
+        request.current_user = auth.current_user(request)
+        if auth.require_auth(
+            request.path, [
+                '/api/v1/status/',
+                '/api/v1/unauthorized/', '/api/v1/forbidden/'
+                ]
+                            ):
+            request.current_user = auth.current_user(request)
+
+            if not auth.authorization_header(request):
+                abort(401)
+            if not auth.current_user(request):
+                abort(403)
+
+
+app.before_request(beforet)
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def error_401(error):
+    """
+    Handles 401 error unauthorized
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def error_403(error):
+    """
+    Handles 403 error unauthorized
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""
+Creates the auth class
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """
+    Hadles authentication
+    return headers
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Check the require paths
+        return headers
+        """
+        if path:
+            if excluded_paths:
+                if path in excluded_paths or (path + "/") in excluded_paths:
+                    return False
+                else:
+                    return True
+            else:
+                return True
+        else:
+            return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Check if the header is authorized
+        and returns headers
+        """
+        if request:
+            auth = request.headers.get('Authorization')
+            return auth
+        else:
+            return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        current user
+        the user
+        """
+        return None

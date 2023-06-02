cfa66f56997d559e8668d6cc09511a2028715523
Kingsley Botchway <mhisterkhing@localhost.localdomain>
2023-06-02 09:25:38 +0000

    commit

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,7 +5,18 @@ import base64
 from typing import TypeVar
 from models.user import User
 from flask import request
+import uuid
 
 
 class SessionAuth(Auth):
     """ Instantiate a basic authentication """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Create a session """
+        if user_id is None or type(user_id) is not str:
+            return None
+        else:
+            sess_id = str(uuid.uuid4())
+            self.user_id_by_session_id[sess_id] = user_id
+            return sess_id

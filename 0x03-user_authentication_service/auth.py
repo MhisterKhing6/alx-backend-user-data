807c0ed27b097d8dacb8bf0a5dd217ea1b594987
Kingsley Botchway <mhisterkhing@localhost.localdomain>
2023-06-06 05:12:44 +0000

    auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 """ authentication """
 import bcrypt
+from db import DB
+from user import User
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Register a user in a database """
+        val1 = self._db._session.query(User).where(
+            User.email == email
+            ).first()
+        if val1 is None:
+            hase = _hash_password(password)
+            return self._db.add_user(email, hase)
+        else:
+            raise ValueError("User {} already exists".format(val1.email))
 
 
 def _hash_password(password: str) -> bytes:
